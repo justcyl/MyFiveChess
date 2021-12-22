2cc45d3a967724940a7c47ae05319ba14307a048
justcyl <chenyilin0760@gmail.com>
2021-12-22 22:39:29 +0800

    depth==4--

--- MyFiveChess/MyFiveChess/MyFiveChess.cpp
+++ MyFiveChess/MyFiveChess/MyFiveChess.cpp
@@ -11,17 +11,19 @@
 #include <windows.h>
 #include <algorithm>
 #include <vector>
+#include <map>
+#include <set>
 
 using namespace std;
 
 
 #define GAP 49
-#define CHESS_SIZE 41
+#define CHESS_SIZE 43
 #define LUPOINT 32
 #define BG_SIZE 749
 
 #define INF 2147483647
-#define MAX_DEP 2
+#define MAX_DEP 4
 
 char Ai_Color = '1';
 
@@ -37,73 +39,131 @@ string Map;
 
 int isGameover = 0;
 
-int lastPos;
-void drawAlpha(IMAGE* picture, int  picture_x, int picture_y); //x为要载入图片的X坐标，y为Y坐标
-void initGraph();
-void drawMap(int lastfill);
-int getClick();
-char xorColor(char ch);
-int evaluateStr(string s);
-int evaluatePos(int pos, char ch);
-int negmax(char color, int alpha, int beta, int depth);
-int getNextMove(int pos);
-void setAi_Color(int x);
-bool isappearFive(string s);
-int evaluateMdia(int x);
-int evaluateSdia(int x);
-void placeChess(int x, char ch);
-void initAI();
+typedef struct Position {
+	int x, y;
+	int score;
 
-struct Point {
-	int p, v;
+	Position() {
+		this->x = 0;
+		this->y = 0;
+		score = 0;
+	}
+	Position(int x, int y) {
+		this->x = x;
+		this->y = y;
+		score = 0;
+	}
+	Position(int x, int y, int score) {
+		this->x = x;
+		this->y = y;
+		this->score = score;
+	}
+	bool operator <(const Position& pos) const {
+		if (score != pos.score) {
+			return score > pos.score;
+		}
+		if (x != pos.x) {
+			return x < pos.x;
+		}
+		else {
+			return y < pos.y;
+		}
+	}
 
-	Point() { 
-		p = 0, v = 0;
-	}
-	Point(int x, int y) {
-		p = x, v = y;
-	}
+}Position;
 
-	bool operator < (const Point& node) const {
-		return v < node.v;
-	}
+
+struct HistoryItem {
+	set<Position> addedPosition;
+	Position removedPosition;
 };
+
+const int fx[] = { 0, 1, 1, 1 };
+const int fy[] = { 1, 0, 1, -1 };
+
+string stringtable[16] =
+{ "11111", "011110", "011100", "001110", "011010", "010110", "11110", "01111", "11011", "10111", "11101", "001100", "001010", "010100", "000100", "001000" };
+
+int scoretable[16] =
+{ 50000, 4320, 720, 720, 720, 720, 720, 720, 720, 720, 720, 120, 120, 120, 20, 20 };
+
+int lastPos;
+inline void drawAlpha(IMAGE* picture, int  picture_x, int picture_y); //x为要载入图片的X坐标，y为Y坐标
+inline void initGraph();
+inline void drawMap(int lastfill);
+inline int getClick();
+inline char xorColor(char ch);
+inline int evaluateStr(string s);
+inline int evaluatePos(int pos, char ch);
+inline int negmax(char color, int alpha, int beta, int depth);
+inline int getNextMove(int pos);
+inline void setAi_Color(int x);
+inline bool isappearFive(string s);
+inline int evaluateMdia(int x);
+inline int evaluateSdia(int x);
+inline void placeChess(int x, char ch);
+inline void initAI();
+inline bool isInBoard(int x, int y);
+inline Position getPos(int x);
+inline int getNum(Position t);
+
+const int nearX[] = { 0, 0, 1, 1, 1, -1, -1, -1};
+const int nearY[] = { 1, -1, 0, 1, -1, 0, 1, -1};
+
+
+class PossiblePositionClass
+{
+public:
+	void AddPossiblePosition(const Position& pos);
+	void Rollback();
+	const set<Position>& GetCurrentPossiblePositions();
+private:
+	set<Position> currentPossiblePositions;
+	vector<HistoryItem> history;
+
+};
+PossiblePositionClass npp;
 
 int main() {
 	
 	initAI();
 	initGraph();
 
-	
+	placeChess(112, (Ai_Color));
+	drawMap(112);
+	npp.AddPossiblePosition(getPos(112));
+
 	while (!isGameover) {
 
-		int player_place = getClick();
+		int player_fill = getClick();
 
 		Fiveexist = false;
-		placeChess(player_place, xorColor(Ai_Color));
+		placeChess(player_fill, xorColor(Ai_Color));
 		//Map[player_fill] = xorColor(Ai_Color);
 
-		drawMap(player_place);
+		drawMap(player_fill);
 
 		if (Fiveexist) {
 			isGameover = xorColor(Ai_Color) - '0';
 			break;
 		}
 
+		npp.AddPossiblePosition(getPos(player_fill));
 		//Now_Score += evaluatePos(6, xorColor(Ai_Color));
-		int AI_place = getNextMove(player_place);
+		int AI_fill = getNextMove(player_fill);
 
 		Fiveexist = false;
-		placeChess(AI_place, (Ai_Color));
+		placeChess(AI_fill, (Ai_Color));
 		//Map[AI_place] = Ai_Color;
 
-		drawMap(AI_place);
+		drawMap(AI_fill);
 
 		//printf("%d", AI_fill);
 		if (Fiveexist) {
 			isGameover = Ai_Color - '0';
 			break;
 		}
+		npp.AddPossiblePosition(getPos(AI_fill));
 	}
 	
 	
@@ -131,9 +191,67 @@ int main() {
 	}*/
 	return 0;
 }
-void initAI() {
+
+
+void PossiblePositionClass::AddPossiblePosition(const Position& pos) {
+	set<Position> addingPositions;
+	for (int i = 0; i < 8; i++) {
+		if (!isInBoard(pos.x + nearX[i], pos.y + nearY[i])) {
+			continue;
+		}
+		Position now(pos.x + nearX[i], pos.y + nearY[i]);
+		if (Map[getNum(now)] == '0') {
+			pair<set<Position>::iterator, bool> insertResult = currentPossiblePositions.insert(now);
+			if (insertResult.second) {
+				addingPositions.insert(now);
+			}
+		}
+	}
+
+	HistoryItem tHi;
+	tHi.addedPosition = addingPositions;
+	
+	if (currentPossiblePositions.find(pos) != currentPossiblePositions.end()) {
+		currentPossiblePositions.erase(pos);
+		tHi.removedPosition = pos;
+	}
+	else {
+		tHi.removedPosition.x = -1;
+	}
+	history.push_back(tHi);
+}
+void PossiblePositionClass::Rollback() {
+	if (currentPossiblePositions.empty()) {
+		return;
+	}
+	HistoryItem tHi = history[history.size() - 1];
+	history.pop_back(); 
+
+	for (set<Position>::iterator iter = tHi.addedPosition.begin(); iter != tHi.addedPosition.end(); iter++) {
+		currentPossiblePositions.erase(*iter);
+	}
+
+	if (tHi.removedPosition.x != -1) {
+		currentPossiblePositions.insert(tHi.removedPosition);
+	}
+}
+const set<Position>& PossiblePositionClass::GetCurrentPossiblePositions() {
+	return currentPossiblePositions;
+}
+
+inline bool isInBoard(int x, int y) {
+	return (x >= 0 && x < 15 && y >= 0 && y < 15);
+}
+inline Position getPos(int x) {
+	Position res(x / 15, x % 15);
+	return res;
+}
+inline int getNum(Position t){
+	return t.x * 15 + t.y;
+}
+inline void initAI() {
 	Map.assign(255, '0');
-	setAi_Color(2);
+	setAi_Color(1);
 
 	for (int i = 0; i < 15; i++) lline[i].append(15, '0'), rrow[i].append(15, '0');
 	for (int i = 1; i < 16; i++) mdia[i].append(14, '0');
@@ -142,11 +260,11 @@ void initAI() {
 	for (int i = 1; i < 14; i++) sdia[i].append(16, '0');
 	sdia[0].append(17, '0');
 }
-char xorColor(char ch){
+inline char xorColor(char ch){
 	if (ch == '0') return ch;
 	else return '3' - ch + '0';
 }
-void initGraph() {
+inline void initGraph() {
 	initgraph(BG_SIZE, BG_SIZE);
 
 	loadimage(&BOARD, L"board.jpg", BG_SIZE, BG_SIZE);
@@ -163,7 +281,7 @@ void initGraph() {
 	putimage(0, 0, &BOARD);
 
 }
-void drawMap(int lastfill) {
+inline void drawMap(int lastfill) {
 	BeginBatchDraw();
 	putimage(0, 0, &BOARD);
 	for (int i = 0; i < 225; i++) {
@@ -183,7 +301,7 @@ void drawMap(int lastfill) {
 
 	EndBatchDraw();
 }
-int getClick()//如果是黑 1  如果是白  2
+inline int getClick()//如果是黑 1  如果是白  2
 {
 	MOUSEMSG msg;
 	while (1)
@@ -210,7 +328,7 @@ int getClick()//如果是黑 1  如果是白  2
 		}
 	}
 }
-void drawAlpha(IMAGE* picture, int  picture_x, int picture_y) //x为载入图片的X坐标，y为Y坐标
+inline void drawAlpha(IMAGE* picture, int  picture_x, int picture_y) //x为载入图片的X坐标，y为Y坐标
 {
 
 	// 变量初始化
@@ -246,18 +364,9 @@ void drawAlpha(IMAGE* picture, int  picture_x, int picture_y) //x为载入图片
 		}
 	}
 }
-int evaluateBoard();
-const int fx[] = {0, 1, 1, 1};
-const int fy[] = {1, 0, 1, -1};
+inline int evaluateBoard();
 
-string stringtable[16] = 
-{"11111", "011110", "011100", "001110", "011010", "010110", "11110", "01111", "11011", "10111", "11101", "001100", "001010", "010100", "000100", "001000"};
-
-int scoretable[16] = 
-{50000, 4320, 720, 720, 720, 720, 720, 720, 720, 720, 720, 120, 120, 120, 20, 20};
-
-
-void setAi_Color(int x) {
+inline void setAi_Color(int x) {
 	Ai_Color = x + '0';
 	for (int i = 0; i < 16; i++) {
 		for (int j = 0; j < stringtable[i].size(); j++) {
@@ -267,14 +376,14 @@ void setAi_Color(int x) {
 }
 
  
-int evaluateBoard() {
+inline int evaluateBoard() {
 	int ans = 0;
 	for (int i = 0; i < 15; i++) ans += evaluateStr(lline[i]) + evaluateStr(rrow[i]);
 	for (int i = 0; i < 16; i++) ans += evaluateMdia(i);
 	for (int i = 0; i < 14; i++) ans += evaluateSdia(i);
 	return ans;
 }
-int evaluateStr(string s){
+inline int evaluateStr(string s){
 	if (s.size() < 5) return 0;
 
 	int begin = -1;
@@ -301,18 +410,18 @@ int evaluateStr(string s){
 	if (isappearFive(s)) Fiveexist = true;
 	return sum;
 }
-bool isappearFive(string s) {
+inline bool isappearFive(string s) {
 	return s.find(stringtable[0], 0) != string::npos;
 }
-int evaluateMdia(int x) {
+inline int evaluateMdia(int x) {
 	if (x == 0 || x == 15) return evaluateStr(mdia[x]);
 	else return evaluateStr(mdia[x].substr(0, 15 - x)) + evaluateStr(mdia[x].substr(15 - x));
 }
-int evaluateSdia(int x) {
+inline int evaluateSdia(int x) {
 	if (x == 0) return evaluateStr(sdia[x].substr(1, 15));
 	else return evaluateStr(sdia[x].substr(0, x)) + evaluateStr(sdia[x].substr(x));
 }
-int evaluatePos(int pos, char ch) {
+inline int evaluatePos(int pos, char ch) {
 	int ans = 0;
 	char t;
 
@@ -342,30 +451,82 @@ int evaluatePos(int pos, char ch) {
 
 	return ans;
 }
-int negmax(char color, int alpha, int beta, int depth) {
+inline int negmax(char color, int alpha, int beta, int depth) {
 	if (depth == MAX_DEP) {
-		//printf("%s %d\n", Map, Now_Score);
 		return evaluateBoard();
-		//return 1;
 	}
-	//int cnt = 0;
+	
+	int cnt = 0;
+	set<Position> PossiblePosition;
+	const set<Position>& tmpPossiblePositions = npp.GetCurrentPossiblePositions();
+
+	for (set<Position>::iterator iter = tmpPossiblePositions.begin(); iter != tmpPossiblePositions.end(); iter++) {
+		PossiblePosition.insert(Position(iter->x, iter->y, evaluatePos(getNum(*iter), color)));
+	}
+
+	while(!PossiblePosition.empty()) {
+		int X = getNum(*PossiblePosition.begin());
+		PossiblePosition.erase(PossiblePosition.begin());
+
+		placeChess(X, color);
+		npp.AddPossiblePosition(getPos(X));
+
+		int val = -negmax(xorColor(color), -beta, -alpha, depth + 1);
+
+		placeChess(X, '0');
+		npp.Rollback();
+
+
+		if (val > alpha) {
+			if (depth == 0) lastPos = X;
+			if (val >= beta) {
+				return beta;
+			}
+			alpha = val;
+		}
+		if (cnt++ > 50) {
+			break;
+		}
+	}
+
+	return alpha;
+}
+
+inline int getNextMove(int pos){
+	lastPos = 1;
+	int val = -negmax((Ai_Color), -INF, INF, 0);
+
+	return lastPos;
+}
+inline void placeChess(int x, char ch) {
+	Map[x] = ch;
+
+	Now_Score += evaluatePos(x, ch);
+
+	lline[x / 15][x % 15] = ch;
+	rrow[x % 15][x / 15] = ch;
+	mdia[x % 16][x / 16] = ch;
+	sdia[x % 14][x / 14] = ch;
+}
+
+//int cnt = 0;
 	//Point* np = new Point[225];
-	/*
-	vector<Point> np;
+
+	/*vector<Position> np;
 	for (int i = 0; i < 225; i++) {
 		if (Map[i] != '0') continue;
 		int delta = -evaluatePos(i, color); //排序从大到小
-		Point anewPoint = Point(i, delta, Fiveexist);
+		Position anewPoint = getPos(i);
 		np.push_back(anewPoint);
 	}
 	sort(np.begin(), np.end());
-	int w = 1, js = 1;
+	/*int w = 1, js = 1;
 
 	while (w < np.size()) {
-		if (np[w - 1].v != np[w].v) js++;	
+		if (np[w - 1].v != np[w].v) js++;
 		w++;
 		if (js == 10) break;
-	}*/
+	}
 	//if (depth == 0)
 	/*
 	vector<Point> np;
@@ -377,38 +538,3 @@ int negmax(char color, int alpha, int beta, int depth) {
 	}
 	sort(np.begin(), np.end());
 	*/
-	for (int i = 0; i < 225; i++) {
-		if (Map[i] != '0') continue;
-		placeChess(i, color);
-		int val = -negmax(xorColor(color), -beta, -alpha, depth + 1);
-		placeChess(i, '0');
-
-
-		if (val > alpha) {
-			if (depth == 0) lastPos = i;
-			if (val >= beta) {
-				return beta;
-			}
-			alpha = val;
-		}
-	}
-
-	return alpha;
-}
-
-int getNextMove(int pos){
-	lastPos = 1;
-	int val = -negmax((Ai_Color), -INF, INF, 0);
-
-	return lastPos;
-}
-void placeChess(int x, char ch) {
-	Map[x] = ch;
-
-	Now_Score += evaluatePos(x, ch);
-
-	lline[x / 15][x % 15] = ch;
-	rrow[x % 15][x / 15] = ch;
-	mdia[x % 16][x / 16] = ch;
-	sdia[x % 14][x / 14] = ch;
-}
